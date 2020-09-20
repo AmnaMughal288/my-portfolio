c0ece6dfbea1dfa588751696faf3489ec55a1e6a
AmnaMughal288 <@mughalamna02@gmail.com>
2020-09-20 14:05:03 -0400

    Updates

---
+++ precache-manifest.11cbee832ee7d172d70643d3883f5bdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540697436c1fb6d61e1c1408db3f323e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a594044f33d860c6f50a",
+    "url": "/my-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "afeddbf3ff6bfc71e82b",
+    "url": "/my-portfolio/static/css/main.f93c5bb1.chunk.css"
+  },
+  {
+    "revision": "a594044f33d860c6f50a",
+    "url": "/my-portfolio/static/js/2.4107b0a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.4107b0a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afeddbf3ff6bfc71e82b",
+    "url": "/my-portfolio/static/js/main.653a47b6.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
